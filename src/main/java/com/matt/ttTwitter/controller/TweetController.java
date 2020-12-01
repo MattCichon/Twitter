f1bb41074c4e8a6f8ca82044fd4d64a9607cdbaa
Matt Cichon <73495007+MattCichon@users.noreply.github.com>
2020-12-01 14:33:05 -0500

    Add files via upload

---
+++ src/main/java/com/matt/ttTwitter/controller/TweetController.java
@@ -0,0 +1,72 @@
+package com.matt.ttTwitter.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.matt.ttTwitter.model.Tag;
+import com.matt.ttTwitter.model.Tweet;
+import com.matt.ttTwitter.model.User;
+import com.matt.ttTwitter.repository.TagRepository;
+import com.matt.ttTwitter.service.TweetService;
+import com.matt.ttTwitter.service.UserService;
+
+@Controller
+public class TweetController {
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private TweetService tweetService;
+
+	@Autowired
+	private TagRepository tagRepository;
+
+	@GetMapping(value = { "/tweets", "/" })
+	public String getFeed(Model model) {
+		List<Tweet> tweets = tweetService.findAll();
+		model.addAttribute("tweetList", tweets);
+		return "feed";
+	}
+
+	@GetMapping(value = "/tweets/new")
+	public String getTweetForm(Model model) {
+		model.addAttribute("tweet", new Tweet());
+		return "newTweet";
+	}
+
+	@GetMapping(value = "/tweets/{tag}")
+	public String getTweetsByTag(@PathVariable(value = "tag") String tag, Model model) {
+		List<Tweet> tweets = tweetService.findAllWithTag(tag);
+		model.addAttribute("tweetList", tweets);
+		model.addAttribute("tag", tag);
+		return "taggedTweets";
+	}
+
+	@GetMapping(value = "/tags")
+	public String getTags(Model model) {
+		List<Tag> tag = (List<Tag>) tagRepository.findAll();
+		model.addAttribute("tagList", tag);
+		return "taggedTweets";
+	}
+
+	@PostMapping(value = "/tweets")
+	public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
+		User user = userService.getLoggedInUser();
+		if (!bindingResult.hasErrors()) {
+			tweet.setUser(user);
+			tweetService.save(tweet);
+			model.addAttribute("successMessage", "Tweet successfully created!");
+			model.addAttribute("tweet", new Tweet());
+		}
+		return "newTweet";
+	}
+}
